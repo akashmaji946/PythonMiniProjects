d324fcfd8060f4dba1ac996008a578e8344a6ee0
AKASH MAJI <akashmaji946@gmail.com>
2020-07-10 05:17:10 +0000

    modified binary search

---
+++ BinarySearchAlternate.py
@@ -0,0 +1,15 @@
+def recursive_binary_search(target, source, left=0):
+    if len(source) == 0:
+        return None
+    center = (len(source)-1) // 2
+    if source[center] == target:
+        return center + left
+    elif source[center] < target:
+        return recursive_binary_search(target, source[center+1:], left+center+1)
+    else:
+        return recursive_binary_search(target, source[:center], left)
+
+
+multiple = [1, 3, 5, 7, 7, 7, 8, 11, 12]
+index = recursive_binary_search(7, multiple)
+print(index)
